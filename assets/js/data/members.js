66298928adffa84b197ea193c7685b08622ec17a
phsfxx <phsilveiraphsf@gmail>
2025-06-05 17:27:41 -0300

    Atualizando informações de Membros

--- assets/js/data/members.js
+++ assets/js/data/members.js
@@ -3,7 +3,7 @@ const members = [{
         name: 'Amanda C. C. L. Bitencourt',
         fullName: 'Amanda Cristine Corrêa Lopes Bitencourt',
         role: 'Coordenadora',
-        shortDescription: 'Possui doutorado em Educação pela Universidade Federal do Rio de Janeiro, mestrado em Educação pela Universidade Católica de Petrópolis, especialização em Reflexão e Análise da Língua Portuguesa e graduação em Letras e em Pedagogia. Atualmente é professora do Departamento de Educação, Linguística e Letras da Universidade do Estado de Minas Gerais e também bolsista de produtividade em pesquisa pela própria instituição. É Professora Formadora da Superintendência de Desenvolvimento de Pessoas da Secretaria de Estado de Educação do Rio de Janeiro - UNIVERSEEDUC. Coordena o Grupo de Pesquisas e Estudos em Currículo, Ensino e Tecnologias - GEPCET/CNPq- UEMG/ Carangola. Coordena Projeto aprovado pela FAPEMIG para o Fortalecimento e Consolidação da Pesquisa na UEMG e UNIMONTES. Atua, principalmente, nos seguintes temas: Currículo; Tecnologias (Digitais) de Informação e Comunicação na Educação; Formação de professores; Educação Bilíngue; Alfabetização.',
+        shortDescription: 'Possui doutorado em Educação pela Universidade Federal do Rio de Janeiro, mestrado em Educação pela Universidade Católica de Petrópolis, especialização em Reflexão e Análise da Língua Portuguesa e graduação em Letras e em Pedagogia. Atualmente, é professora do Departamento de Educação, Linguística e Letras da Universidade do Estado de Minas Gerais, onde também atua como bolsista de produtividade em pesquisa. É Professora Formadora da Superintendência de Desenvolvimento de Pessoas da Secretaria de Estado de Educação do Rio de Janeiro – UNIVERSEEDUC. Coordena o Grupo de Pesquisas e Estudos em Currículo, Ensino e Tecnologias – GEPCET/CNPq-UEMG/Carangola e o Projeto aprovado pela FAPEMIG voltado ao fortalecimento e à consolidação da pesquisa na UEMG e UNIMONTES. Atua principalmente nos seguintes temas: currículo, tecnologias (digitais) de informação e comunicação na educação, letramento e educação bilíngue.',
         image: '../pages/assets/imagens/fotoMembros/membroAmanda.jpg',
         academicFormation: [
             'Doutora em Educação (UFRJ)',
@@ -15,9 +15,8 @@ const members = [{
         areas: [
             'Currículo',
             'Tecnologias Digitais de Informação e Comunicação na Educação',
-            'Formação de Professores',
-            'Educação Bilíngue',
-            'Alfabetização'
+            'Letramento',
+            'Educação Bilíngue'
         ],
         lattesUrl: 'http://lattes.cnpq.br/2737467410088287',
         email: 'amanda.bitencourt@uemg.br'
@@ -27,7 +26,7 @@ const members = [{
         name: 'Nilton Freitas',
         fullName: 'Nilton Freitas Júnior',
         role: 'Coordenador',
-        shortDescription: 'Graduação em Sistemas de Informação pelo Centro Universitário Faminas. Pós-graduação em Engenharia de Software pela Universidade Gama Filho, pós-graduação em Planejamento, Implementação e Gestão da EAD pela Universidade Federal Fluminense. Mestrado em Pesquisa Operacional e Inteligência Computacional pela Universidade Cândido Mendes, em Campos dos Goytacazes. Atualmente é professor do Centro Universitário Faminas, campus Muriaé, lecionando disciplinas relacionadas à Tecnologia da Informação para diversos cursos de graduação; também é professor designado na Universidade do Estado de Minas Gerais - UEMG - Unidade Carangola, para o curso de Sistemas de Informação. Tem longa experiência na área de Computação, com ênfase em Sistemas de Informação, Análise e Desenvolvimento de Sistemas, Linguagens de Programação, Algoritmos e Estruturas de Dados, além de conhecimento técnico em diversas plataformas de hardware e software, domínio de linguagens de programação de construção estruturada e orientação a objetos. Experiência docente superior a 25 anos, em ambientes acadêmicos diversos. Experiência e versatilidade para atuação em ambientes de Educação à Distância, Ensino Remoto e aplicações de Inteligência Artificial no ambiente educacional.',
+        shortDescription: 'Possui graduação em Sistemas de Informação pelo Centro Universitário Faminas, pós-graduação em Engenharia de Software pela Universidade Gama Filho e pós-graduação em Planejamento, Implementação e Gestão da Educação a Distância pela Universidade Federal Fluminense. Mestrado em Pesquisa Operacional e Inteligência Computacional pela Universidade Cândido Mendes, em Campos dos Goytacazes. Atualmente, é professor do Centro Universitário Faminas, campus Muriaé, lecionando disciplinas relacionadas à Tecnologia da Informação para diversos cursos de graduação, além de atuar como professor designado na Universidade do Estado de Minas Gerais – UEMG, Unidade Carangola, no curso de Sistemas de Informação. Tem experiência na área de Computação, com ênfase em Sistemas de Informação, Análise e Desenvolvimento de Sistemas, Linguagens de Programação, Algoritmos e Estruturas de Dados, além de conhecimento técnico em diversas plataformas de hardware e software, domínio de linguagens de programação de construção estruturada e orientação a objetos. Acumula mais de 25 anos de experiência docente em ambientes acadêmicos diversos, com atuação em Educação a Distância, Ensino Remoto e aplicações de Inteligência Artificial no ambiente educacional.',
         image: '../pages/assets/imagens/fotoMembros/membroNilton.jpg',
         academicFormation: [
             'Graduação em Sistemas de Informação (Faminas)',
@@ -44,14 +43,59 @@ const members = [{
             'Inteligência Artificial na Educação'
         ],
         lattesUrl: 'http://lattes.cnpq.br/3384614855085107',
-        email: 'amanda.bitencourt@uemg.br'
+        email: 'nilton.freitas@uemg.br'
+    },
+    {
+        id: 'clarete',
+        name: 'Clarete Aparecida Diniz Gomes',
+        fullName: 'Clarete Aparecida Diniz Gomes',
+        role: 'Professora',
+        shortDescription: 'Possui formação acadêmica compatível com sua atuação na área educacional, incluindo graduação, especialização, mestrado e doutorado em instituições reconhecidas. Atualmente, exerce atividades docentes e de pesquisa em instituições de ensino superior, contribuindo para a formação de profissionais e o desenvolvimento de estudos na área. Sua atuação abrange temáticas relacionadas ao ensino, currículo, metodologias de aprendizagem e inovação educacional, com ênfase na aplicação de tecnologias digitais no processo de ensino e aprendizagem.',
+        image: '../pages/assets/imagens/fotoMembros/membroClarete.jpg',
+        academicFormation: [
+            'Graduação em Sistemas de Informação (UEMG - Carangola)',
+            'Especialização em Planejamento, Implementação e Gestão da EAD (UFF)',
+            'Especialista em Gerenciamento de Projetos - TI (FACUMINAS)',
+            'Especialista em Informática na Educação (IFES)',
+            'Mestrado em andamento em Informática (UFES)'
+        ],
+        areas: [
+            'Tecnologia na Educação',
+            'Gerenciamento de Projetos',
+            'Informática na Educação',
+            'Educação a Distância',
+            'Sistemas de Informação'
+        ],
+        lattesUrl: 'http://lattes.cnpq.br/1115969730131867',
+        email: 'clarete.diniz@uemg.br'
+    },
+    {
+        id: 'pedro',
+        name: 'Pedro Henrique Silveira Fonseca',
+        fullName: 'Pedro Henrique Silveira Fonseca',
+        role: 'Estudante',
+        shortDescription: 'Estudante de Sistemas de Informação na Universidade do Estado de Minas Gerais – UEMG, Unidade Carangola – MG. Possui experiência em Ciência da Computação, com ênfase em desenvolvimento de sistemas, análise de dados e tecnologias educacionais. Participou de projetos de pesquisa, como o PAEx: Currículo, Ensino Híbrido e Cultura Maker, voltados para práticas pedagógicas inovadoras e a integração de tecnologias na educação básica. Atua principalmente nos seguintes temas: metodologias ativas, cultura maker e desenvolvimento de soluções tecnológicas aplicadas.',
+        image: '../pages/assets/imagens/fotoMembros/membroPedro.jpg',
+        academicFormation: [
+            'Graduando em Sistemas de Informação (UEMG - Carangola)'
+        ],
+        areas: [
+            'Desenvolvimento de Sistemas',
+            'Análise de Dados',
+            'Tecnologias Educacionais',
+            'Metodologias Ativas',
+            'Cultura Maker',
+            'Soluções Tecnológicas Aplicadas'
+        ],
+        lattesUrl: 'https://wwws.cnpq.br/cvlattesweb/PKG_MENU.menu?f_cod=C8759871268F8901B0A775139E04544D#',
+        email: 'pedro.1295239@discente.uemg.br'
     },
     {
         id: 'tome',
         name: 'Tomé Mata de Freitas',
         fullName: 'Tomé Mata de Freitas',
         role: 'Pesquisador',
-        shortDescription: 'Tomé Mata de Freitas é graduado em Administração e em Sistemas de Informação pela Universidade do Estado de Minas Gerais – UEMG (Unidade Carangola). É pós-graduado em Inteligência Artificial pela Facuminas e atua na interseção entre tecnologia e ensino. Atualmente, cursa Pedagogia pela Unicesumar, onde também integra o Programa Institucional de Bolsas de Iniciação Científica (PIBIC). Está em fase de conclusão da pós-graduação em Educação Inclusiva, também pela Facuminas, desenvolvendo pesquisas voltadas à aplicação de tecnologias na promoção de práticas inclusivas no ensino. Seus principais interesses envolvem o uso ético e responsável da tecnologia no processo de ensino-aprendizagem, metodologias ativas de ensino e o desenvolvimento de tecnologias assistivas voltadas à educação inclusiva.',
+        shortDescription: 'Possui graduação em Administração e em Sistemas de Informação pela Universidade do Estado de Minas Gerais – UEMG, Unidade Carangola, e pós-graduação em Inteligência Artificial pela Facuminas. Atualmente, cursa Pedagogia pela Unicesumar, onde integra o Programa Institucional de Bolsas de Iniciação Científica (PIBIC). Está em fase de conclusão da pós-graduação em Educação Inclusiva pela Facuminas, desenvolvendo pesquisas voltadas à aplicação de tecnologias na promoção de práticas inclusivas no ensino. Atua principalmente nos seguintes temas: uso ético e responsável da tecnologia no processo de ensino-aprendizagem, metodologias ativas de ensino e desenvolvimento de tecnologias assistivas voltadas à educação inclusiva.',
         image: '../pages/assets/imagens/fotoMembros/membroTome.jpg',
         academicFormation: [
             'Graduado em Administração (UEMG - Carangola)',
@@ -72,49 +116,41 @@ const members = [{
         email: 'tome.1216086@discente.uemg.br'
     },
     {
-        id: 'pedro',
-        name: 'Pedro Henrique Silveira Fonseca',
-        fullName: 'Pedro Henrique Silveira Fonseca',
+        id: 'nilson',
+        name: 'Nilson Alves de Andrade',
+        fullName: 'Nilson Alves de Andrade',
         role: 'Estudante',
-        shortDescription: 'Estudante de Sistemas de Informação UEMG Unidade Carangola - MG. Possui experiência em Ciência da Computação, com ênfase em áreas como desenvolvimento de sistemas, análise de dados e tecnologias educacionais. Participou de projetos de pesquisa, como o PAEx: Currículo, Ensino Híbrido e Cultura Maker, voltados para práticas pedagógicas inovadoras e a integração de tecnologias na educação básica. Interesse em metodologias ativas, cultura maker e desenvolvimento de soluções tecnológicas aplicadas.',
-        image: '../pages/assets/imagens/fotoMembros/membroPedro.jpg',
+        shortDescription: 'Estudante de Licenciatura em Letras – Português e Inglês pela UEMG – Unidade de Carangola. Atuou como monitor da disciplina Língua Inglesa II no curso de Licenciatura em Letras - Português e Inglês pela UEMG durante o segundo semestre letivo de 2024. Foi voluntário no projeto de pesquisa e extensão Copia não, comédia!, coordenado pela Prof.ª Dr.ª Christiane Miranda Buthers de Almeida durante o primeiro semestre letivo de 2024. Atualmente bolsista do Projeto de Pesquisas e Estudos em Currículo, Ensino e Tecnologias – GEPCET.',
+        image: '../pages/assets/imagens/fotoMembros/membroNilson.jpg',
         academicFormation: [
-            'Graduando em Sistemas de Informação (UEMG - Carangola)'
+            'Graduando em Licenciatura em Letras – Português e Inglês (UEMG - Carangola)'
+        ],
+        areas: [
+            'Educação Básica',
+            'Ensino de Língua Estrangeira',
+            'Elaboração de material didático',
+            'Educação inclusiva'
+        ],
+        lattesUrl: 'http://lattes.cnpq.br/8167164205374943',
+        email: 'nilson.1295103@discente.uemg.br'
+    },
+    {
+        id: 'gisella',
+        name: 'Gisella Miranda Garcia',
+        fullName: 'Gisella Miranda Garcia',
+        role: 'Estudante',
+        shortDescription: 'Possui ensino médio pela Escola Estadual Coronel Rezende (2022). Tem experiência na área de Ciência da Computação, com ênfase em metodologia e técnicas da computação. Atua principalmente nos seguintes temas: desenvolvimento de sistemas, análise de dados e integração de tecnologias na educação.',
+        image: '../pages/assets/imagens/fotoMembros/membroGisella.jpg',
+        academicFormation: [
+            'Ensino Médio (EE Coronel Rezende - 2022)'
         ],
         areas: [
             'Desenvolvimento de Sistemas',
             'Análise de Dados',
-            'Tecnologias Educacionais',
-            'Metodologias Ativas',
-            'Cultura Maker',
-            'Soluções Tecnológicas Aplicadas'
+            'Integração de Tecnologias na Educação'
         ],
-        lattesUrl: 'https://wwws.cnpq.br/cvlattesweb/PKG_MENU.menu?f_cod=C8759871268F8901B0A775139E04544D#',
-        email: 'pedro.1295239@discente.uemg.br'
-    },
-    {
-        id: 'clarete',
-        name: 'Clarete Aparecida Diniz Gomes',
-        fullName: 'Clarete Aparecida Diniz Gomes',
-        role: 'Professora',
-        shortDescription: 'Sou graduada em Sistemas de Informação pela UEMG - Campus Carangola (2009), possuo especialização em Planejamento, Implementação e Gestão da Ead Pela UFF, especialista em Gerenciamento de Projetos - TI pela FACUMINAS, e em Informática na Educação pelo IFES e em andamento com o Mestrado em Informática na UFES. Sou professora universitária na UEMG - Campus Carangola com pesquisas na área de tecnologia na educação.',
-        image: '../pages/assets/imagens/fotoMembros/membroClarete.jpg',
-        academicFormation: [
-            'Graduada em Sistemas de Informação (UEMG - Carangola)',
-            'Especialização em Planejamento, Implementação e Gestão da EAD (UFF)',
-            'Especialista em Gerenciamento de Projetos - TI (FACUMINAS)',
-            'Especialista em Informática na Educação (IFES)',
-            'Mestrado em andamento em Informática (UFES)'
-        ],
-        areas: [
-            'Tecnologia na Educação',
-            'Gerenciamento de Projetos',
-            'Informática na Educação',
-            'Educação a Distância',
-            'Sistemas de Informação'
-        ],
-        lattesUrl: 'http://lattes.cnpq.br/1115969730131867',
-        email: 'clarete.diniz@uemg.br'
+        lattesUrl: 'http://lattes.cnpq.br/1314238092568154',
+        email: 'gisella.241121889@discente.uemg.br'
     }
 ];
 
